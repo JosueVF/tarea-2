bef86df4995da9018f14cf5589bcb586dbd3deb6
JosueVF <95833498+JosueVF@users.noreply.github.com>
2023-02-26 20:54:24 -0600

    Add files via upload

---
+++ ex2/lib/Math.js
@@ -0,0 +1,45 @@
+class Math {
+    /**
+     * add
+     * @param {number} a the first number to add
+     * @param {number} b the second number to add 
+     * @returns {number} the sum of a and b
+     */
+    add (a, b) {
+      if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('both numbers must be type number')
+      }
+      return a + b;
+    }
+  
+    /**
+     * sumArr
+     * @param {Array<number>} arr the array of numbers to add
+     */
+    sumArr (arr) {
+      let sum = 0;
+      for (let i=0; i < arr.length; i++) {
+        sum += arr[i];
+      }
+      return sum;
+    }
+
+    avg (arr){
+      const sum = this.sumArr(arr);
+      return sum/arr.length;
+    }
+
+    uniques (arr){
+      const nArr = [];
+
+      for (let i=0; i < arr.length; i++){
+        const element = arr[i];
+        if (!nArr.includes(element)){
+          nArr.push(element);
+        }
+      }
+      return nArr;
+    }
+  }
+  
+  module.exports = Math;
